16a13c09ebe36756fb9516a93fd6b786be7cfee1
Mohamed-N-Mohamed <Mohamed-N-Mohamed@outlook.com>
2020-05-13 00:29:28 +0100

    updated files

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ async function fetchAnime(e) {
   e.preventDefault();
   if (search.value.trim() === "") {
     //show error
-    alert("you need to type something");
+    showError();
   } else {
     //fetch data
     try {
@@ -40,7 +40,7 @@ function createDOM(data) {
     return acc;
   }, {});
 
-
+  //loop through each categories anime and sort them and insert DOM elements to the website
   content.innerHTML = Object.keys(animeByCategories)
     .map((key) => {
       const animeDOM = animeByCategories[key]
@@ -50,27 +50,48 @@ function createDOM(data) {
         .map((anime) => {
           return `
    
-    <div class="card" style="width: 18rem;">
+    <div class="card">
    <img src="${anime.image_url}" class="card-img-top" alt="...">
    <div class="card-body">
      <h5 class="card-title">${anime.title}</h5>
      <p class="card-text">${anime.synopsis}</p>
-     <a href="${anime.url}" class="btn btn-primary">Go somewhere</a>
+     <a href="${anime.url}" class="btn btn-primary" target="_blank">Find out more</a>
    </div>
  </div>
       `;
         })
         .join("");
 
-      return ` <section>
+      return ` <div>
       <h2>${key.toUpperCase()}</h2>
       <div class="my-grid">${animeDOM}</div>
 
-              </section>
+              </div>
       
       `;
     })
-    .join('');
+    .join("");
 }
+
+//create error when nothing is typed
+function showError() {
+  //clear error from duplicating
+  removeError();
+  //create error div
+  let error = document.createElement("div");
+  error.className = "alert alert-danger";
+  error.appendChild(document.createTextNode("You did not type anything!!"));
+  content.append(error);
+  //clear error after 3 secs
+  setTimeout(removeError, 3000);
+}
+
+function removeError() {
+  let current = document.querySelector(".alert");
+  if (current) {
+    current.remove();
+  }
+}
+
 //listen for submit
 submit.addEventListener("click", fetchAnime);
